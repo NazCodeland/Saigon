d2ea617be6cfe7d1c9908e473a2cf39a2ed87ba0
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-12-30 22:19:48 -0500

    install adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import ("@sveltejs/kit").Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	vitePlugin: {
-		inspector: true
-	},
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: '.svelte-kit/cloudflare'
+		})
 	}
 };
 
