ad646ceb1505348410c9ba2f42406318e24f93b7
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-12-31 02:00:00 -0500

    add landscape styles to the hero image & logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,13 +4,16 @@
 	let imageHeight: number;
 </script>
 
-<header class="md:grid md:items-center" style="height: {imageHeight}px;">
+<header
+	class="md:grid md:items-center landscape:m-auto landscape:-mt-4 landscape:w-[132vh]"
+	style="height: {imageHeight}px;"
+>
 	<Logo />
 
 	<!-- hero image -->
 	<div bind:clientHeight={imageHeight} class="absolute right-0 top-0 -z-10 w-full">
 		<img
-			class="m-auto object-contain object-top md:max-h-[100vh]"
+			class="m-auto object-contain object-top md:max-h-[100vh] landscape:h-[86vh]"
 			src="hero-image.jpg"
 			alt="Saigon Logo"
 		/>
