35525bf2c94a2000c2e118b5556b1b0668a97b9e
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-01-01 10:35:43 -0500

    fix: 'Hour & Location' to 'Hours & Location'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,8 @@
 	class="m-auto flex max-w-[1230px] flex-col gap-32 p-4 font-restaurant text-base text-theme-text"
 >
 	<div>
-		<!-- <Header /> -->
-		<!-- <Menu /> -->
+		<Header />
+		<Menu />
 	</div>
 
 	<section class="grid gap-16 rounded-md" id="hours">
@@ -20,7 +20,7 @@
 			class="text-center text-3xl font-bold selection:bg-[#FFFF00]
 			selection:text-theme-background"
 		>
-			Hour & Location
+			Hours & Location
 		</h2>
 		<div class="flex flex-wrap justify-center gap-8">
 			<Hours />
@@ -33,12 +33,12 @@
 			class=" text-center text-3xl font-bold selection:bg-[#FFFF00]
 			selection:text-theme-background"
 		>
-			Contact
+			<!-- Contact -->
 		</h2>
-		<Contact />
+		<!-- <Contact /> -->
 	</section>
 
-	<Footer />
+	<!-- <Footer /> -->
 </main>
 
 <style>
