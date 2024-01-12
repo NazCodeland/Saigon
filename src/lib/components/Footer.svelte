aa5549744997962b61d80407f91851547ec10927
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-01-12 17:06:04 -0500

    change externalLink icon size and Footer.svelte text size and alignment

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,9 +2,14 @@
 	import externalLink from '$lib/icons/externalLink.svg';
 </script>
 
-<footer class="ml-auto">
-	&copy; 2024 <strong>Saigon Ottawa</strong> | Site by
-	<a class="text-[#6095F5] hover:underline" href="http://nazcodeland.dev"
-		>NazCodeland <img class="inline-block" src={externalLink} alt="" /></a
-	>
+<footer class="ml-auto text-sm">
+	<span>
+		&copy; 2024 <strong>Saigon</strong>
+	</span>
+	<span>
+		| Site by
+		<a class="inline-flex gap-0.5 text-[#6095F5] hover:underline" href="http://NazCodeland.dev">
+			NazCodeland <img class="inline-block" src={externalLink} alt="" />
+		</a>
+	</span>
 </footer>
