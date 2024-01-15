043024560c9011af685627bbaf089862d08ec4e2
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-01-15 00:06:33 -0500

    modify footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,12 +9,29 @@
 	<span>
 		| Site by
 		<a
-			class="inline-flex text-[#6095F5] hover:underline"
+			class="inline-flex items-center border-b border-[#6095F5]"
 			href="http://NazCodeland.dev"
 			rel="noopener noreferrer"
 			target="_blank"
 		>
-			NazCodeland <img class="inline-block" src={externalLink} alt="" />
+			NazCodeland
+
+			<!-- external link icon -->
+			<svg
+				width="14px"
+				height="14px"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M7 17L17 7M17 7H8M17 7V16"
+					stroke="#6095F5"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				></path>
+			</svg>
 		</a>
 	</span>
 </footer>
